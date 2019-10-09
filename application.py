7d6706f1d8cb9d7024798da63b418564deef69ed
Siyu Zhang <z1s1y8@gmail.com>
2019-10-09 14:20:16 -0400

    start small

--- application.py
+++ application.py
@@ -9,12 +9,12 @@ Created on Wed Oct  9 11:28:53 2019
 #import os
 from flask import Flask, render_template, request, redirect
 
-app = Flask(__name__, instance_relative_config=True)
+app = Flask(__name__)
 
-app.config.from_mapping(
-    SECRET_KEY='dev',
+#app.config.from_mapping(
+#    SECRET_KEY='dev',
 #    DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
-    )
+#    )
 #if test_config is None:
     # load the instance config, if it exists, when not testing
 #    app.config.from_pyfile('config.py', silent=True)
